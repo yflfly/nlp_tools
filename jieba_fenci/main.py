8deb02f3d636d81579d83fe948cf81ef1cd669d0
杨凤玲 <yangfengling@inttech.cn>
2020-04-21 14:45:05 +0800

    中文jieba分词

--- jieba_fenci/main.py
+++ jieba_fenci/main.py
@@ -1,5 +1,7 @@
 # coding:utf-8
 import jieba
+
+
 # 对一句话进行分词
 def handle_1():
     string = '自然语言处理NLP'
@@ -14,4 +16,24 @@ def handle_1():
     运行的结果如下所示：
     自然语言 处理 NLP
     ['自然语言', '处理', 'NLP']
+    '''
+
+
+# 词性分析
+import jieba.posseg as pos
+
+
+def handle_2():
+    string = '自然语言处理NLP'
+    # generator形式形如pair(‘word’, ‘pos’)的结果
+    seg = pos.cut(string)
+    print([se for se in seg])
+
+    # list形式的结果
+    seg = pos.lcut(string)
+    print(seg)
+    '''
+    运行的结果如下所示：
+    [pair('自然语言', 'l'), pair('处理', 'v'), pair('NLP', 'eng')]
+    [pair('自然语言', 'l'), pair('处理', 'v'), pair('NLP', 'eng')]
     '''
